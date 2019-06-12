ebfcdc7eba93703f9bf2295aa4979387560cecc3
github username <github email address>
2019-06-11 21:37:32 -0400

    Fetch from API

--- weather-app/src/component/weeks.js
+++ weather-app/src/component/weeks.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
+// import API from "./apiKeys"
 
 class Weeks extends Component {
+  componentDidMount = () => {
+    const weatherURL = "http://api.openweathermap.org/data/2.5/forecast?zip=10002&units=imperial&APPID=35a1b679220928dae82f450b592b3e22"
+
+    fetch(weatherURL)
+      .then(res => res.json())
+      .then(data =>
+        console.log(data.list)
+      )
+  }
+
   render() {
     return(
       <div> HI </div>
