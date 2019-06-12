463a740388be76d5da06aa5e11b3ee6d5219c7bd
github username <github email address>
2019-06-12 04:17:18 -0400

    Add cards to the week container

---
+++ weather-app/src/component/day.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Day extends Component {
+  render() {
+    return(
+      <h1> Day Card</h1>
+    )
+  }
+}
+
+export default Day;
--- weather-app/src/component/weeks.js
+++ weather-app/src/component/weeks.js
@@ -1,20 +1,39 @@
 import React, { Component } from "react";
+import Day from "./day"
 // import API from "./apiKeys"
 
 class Weeks extends Component {
+  state = {
+    fullData: [],
+    dailyData: []
+  }
+
   componentDidMount = () => {
     const weatherURL = "http://api.openweathermap.org/data/2.5/forecast?zip=10002&units=imperial&APPID=35a1b679220928dae82f450b592b3e22"
 
     fetch(weatherURL)
       .then(res => res.json())
-      .then(data =>
-        console.log(data.list)
-      )
+      .then(data => {
+        const dailyData = data.list.filter(reading => {
+          return reading.dt_txt.includes("03:00:00")
+        })
+
+        this.setState({
+          fullData: data,
+          dailyData: dailyData
+        }, () => console.log(this.state))
+      })
+  }
+
+  dayCards = () => {
+    return this.state.dailyData.map((reading, idx) => {
+      return <Day reading={reading} key={idx} />
+    })
   }
 
   render() {
     return(
-      <div> HI </div>
+      <div>{this.dayCards()}</div>
     )
   }
 }
